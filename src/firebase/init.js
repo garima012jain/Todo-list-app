00d05aa4ba802e93b2c52df7c904f2fcdc6baaed
garima012jain <43791459+garima012jain@users.noreply.github.com>
2020-06-08 04:52:48 +0000

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,14 +3,14 @@ import '@firebase/firestore'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC2oZNBdt46gKpIoFBM--JGT0_HtmLsZc8",
-    authDomain: "todolist-d3f34.firebaseapp.com",
-    databaseURL: "https://todolist-d3f34.firebaseio.com",
-    projectId: "todolist-d3f34",
-    storageBucket: "todolist-d3f34.appspot.com",
-    messagingSenderId: "1043392272339",
-    appId: "1:1043392272339:web:95a5ac85108bc1f7340126",
-    measurementId: "G-FJYFT8EBLK"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
+  databaseURL: "https://" + process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseio.com',
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.appspot.com',
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 //Initialize Firebase
 //firebase.initializeApp(firebaseConfig);
